4b053de4b46739816a4fa88be2a042b6764f2ae4
Maah-aquino <107276019+Maah-aquino@users.noreply.github.com>
2022-11-22 14:27:27 -0300

    Update livro.cpp

---
+++ src/livro.cpp
@@ -0,0 +1,8 @@
+void Livro::exibirInformacoes(){
+	cout << "Identificador: " << Livro::getIdLivro() << endl;
+		cout << "Autor" << Livro::getAUtorLivro() << endl;
+	cout << "Título: " << Livro::getNomeLivro()  << endl;
+	cout << "Edição: " << Livro::getEdicaoLivro() << endl;
+	cout << "Assunto: " << Livro::getAssuntoLivro() << endl;
+	cout << "Quantidade Total: " << Livro::getQtdeLivro() << endl;
+	
